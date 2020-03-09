370b67585688f8f70cef9462b9a602cfdf1c2545
fl <984460054@qq.com>
2020-03-09 15:19:35 +0800

    init report

---
+++ src/main/java/Star.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fule
+ * @descriptionStart
+ * @date3-9
+ */
+
+@SpringBootApplication
+public class Star {
+    public static void main(String[] args) {
+        SpringApplication.run(Star.class, args);
+    }
+}
